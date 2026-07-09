ace040a059f4d0d6069d284dcf5bbf718f01d0d1
rafsanx <developer.rafsanx@gmail.com>
2026-07-09 22:23:50 +0600

    PWA Apk add

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -10,6 +10,7 @@ export default function manifest() {
     icons: [
       { src: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { src: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+      { src: "/icon-1024x1024.png", sizes: "1024x1024", type: "image/png", purpose: "any maskable" },
     ],
   }
 }
